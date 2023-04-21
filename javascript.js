966c32a78f94589dc9891608402cf4182141e356
Patsch03 <pschenk0304@gmail.com>
2023-04-21 16:12:23 -0400

    api practice

---
+++ javascript.js
@@ -0,0 +1,8 @@
+// const fetch = require("node-fetch");
+
+// async function fetchJoke() { 
+//     const results = await fetch(`https://api.chucknorris.io/jokes/random`);
+//     const data = await results.json();
+//         console.log(data.value);
+// } 
+// fetchJoke();
