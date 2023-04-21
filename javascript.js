9461ae3c1e2438cb29427b3ff7ac3a61d521b571
Patsch03 <pschenk0304@gmail.com>
2023-04-21 16:26:23 -0400

    Tic tac toe, entering input

--- javascript.js
+++ javascript.js
@@ -1,14 +1,43 @@
 // const fetch = require("node-fetch");
 
+const prompt = require('prompt-sync')();
+
+
 // async function fetchJoke() { 
 //     const results = await fetch(`https://api.chucknorris.io/jokes/random`);
 //     const data = await results.json();
 //         console.log(data.value);
 // } 
 // fetchJoke();
-
-
+winStatus = false;
+colMove = "";
+rowMove = "";
 
 let board = [[0,0,0],
              [0,0,0],
              [0,0,0]];
+
+
+
+
+
+function checkWin(){
+    if(colMove == "x" || rowMove == "x"){
+        winStatus = true;
+    }
+
+    return winStatus;
+}
+
+
+function play(){
+    while(!winStatus){
+        colMove = prompt("Choose a column");
+        rowMove = prompt("Choose a row");
+        console.log(colMove);
+        console.log(rowMove);
+        checkWin();
+    }
+}
+
+play();
